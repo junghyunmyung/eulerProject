3c89bf1dbec2c4e7077d94f84491440b2f6ecbe4
wise <zpqq01@naver.com>
2023-07-14 19:23:12 +0900

    1day

---
+++ test1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		int i = 0, j= 0;
+		for (i= 0; i<1000; i++) {
+			if(i%3==0|| i%5==0) {
+				j = j+i;
+			}
+		}
+		System.out.println(j);
+	}
+
+}
